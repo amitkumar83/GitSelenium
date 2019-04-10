438381dc128c970f3acb316e94018f765bd60ed7
amit <amitmurarka83@gmail.com>
2019-04-10 17:01:20 +0530

    final commit Person Y

--- src/test/java/seleniumTest/Demo4Test.java
+++ src/test/java/seleniumTest/Demo4Test.java
@@ -1,5 +1,16 @@
 package seleniumTest;
 
+import org.testng.annotations.Test;
+
 public class Demo4Test {
 
+	@Test
+	public void DemoD1(){
+		System.out.println("I am DemoD1");
+	}
+	
+	@Test
+	public void DemoD2(){
+		System.out.println("I am DemoD2");
+	}
 }
